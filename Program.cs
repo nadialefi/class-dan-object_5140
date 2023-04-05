cf09b342f64825b94d5bcb9b3510543ad92a81f3
nadialefi <128023715+nadialefi@users.noreply.github.com>
2023-04-05 11:52:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Taxi taxi = new Taxi();
+
+            taxi.DriveName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasTaxi
+{
+    public class Taxi
+    {
+        public string DriveName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Drive Name : {0}", DriveName);
+            if (OnDuty)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriveName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriveName);
+        }
+    }
+}
